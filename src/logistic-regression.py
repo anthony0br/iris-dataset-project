829c76eb3d44c427786b8af2d82cda56b0ccfc27
anthony0br <129159900+anthony0br@users.noreply.github.com>
2023-08-11 16:34:44 +0100

    Imported LogisticRegression

---
+++ src/logistic-regression.py
@@ -0,0 +1,5 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.linear_model import LogisticRegression
